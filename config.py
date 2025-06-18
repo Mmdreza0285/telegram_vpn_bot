818f404ea1aafbc95898fe9d196c4170f58b7972
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 03:23:45 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "").split(",") if admin_id]
