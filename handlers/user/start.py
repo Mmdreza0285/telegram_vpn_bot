59d6f2103e08926c0b969778d789857ffa37aece
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 03:27:32 +0330

    Create start.py

---
+++ handlers/user/start.py
@@ -0,0 +1,20 @@
+# handlers/user/start.py
+
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from keyboards.user_menu import user_main_menu
+from database.db import create_user_if_not_exists
+
+router = Router()
+
+@router.message(CommandStart())
+async def start_handler(message: Message):
+    await create_user_if_not_exists(message.from_user.id)
+
+    await message.answer(
+        f"سلام {message.from_user.full_name} 👋\n"
+        "به ربات فیلترشکن خوش اومدی!\n"
+        "از منوی زیر یکی از گزینه‌ها رو انتخاب کن:",
+        reply_markup=user_main_menu()
+    )
