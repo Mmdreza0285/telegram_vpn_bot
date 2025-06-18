f1e767def7dddfd4ea194c6f427e87df59b47951
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 01:43:56 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,9 @@
 from aiogram import Router, types
-from database.db import add_user
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
 router = Router()
 
-@router.message(commands=["start"])
-async def start_handler(msg: types.Message):
-    ref = msg.text.split(" ")[1] if len(msg.text.split()) > 1 else None
-    add_user(msg.from_user.id, msg.from_user.username, ref)
-    await msg.answer(f"سلام {msg.from_user.full_name} عزیز 🌟\nبه ربات خوش اومدی!")
+@router.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("سلام! به ربات کانفیگ VPN خوش اومدی.")
