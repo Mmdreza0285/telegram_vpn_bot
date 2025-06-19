36c722c6a9e1323d46834e8cd1343baf1509ee36
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 21:18:11 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,12 @@
-from aiogram import Router, types
+from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from keyboards.user_menu import user_main_menu
 
 router = Router()
 
-@router.message(CommandStart())
+@router.message(commands=["start"])
 async def start_handler(message: Message):
-    await message.answer("سلام! به ربات کانفیگ VPN خوش اومدی.")
+    await message.answer(
+        "سلام به ربات ShadowVPN خوش اومدی! 😎\nاز منوی زیر گزینه مورد نظرت رو انتخاب کن.",
+        reply_markup=user_main_menu()
+    )
