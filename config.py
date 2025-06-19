bd68e43aead28b9dae8f08a321750ad61cea8fe2
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 06:43:10 +0330

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "").split(",") if admin_id]
+BOT_USERNAME = "shadoowvpnnbot"  # آیدی ربات بدون @
