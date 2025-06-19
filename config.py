b64f7380f4278357a520f1bc56fa53eca0dd742e
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 22:16:43 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-# config.py
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "").split(",") if admin_id]
-BOT_USERNAME = "shadoowvpnnbot"  # آیدی ربات بدون @
+ADMINS = [int(i) for i in os.getenv("ADMINS", "").split(",")]
+CHANNEL_ID = os.getenv("CHANNEL_ID")
