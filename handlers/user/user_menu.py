92a8c2870e1eadc20b5b8fb4b295a8241f79d2f9
shadoowvpnn <mohammadreza138112@gmail.com>
2025-06-19 21:18:58 +0330

    Update user_menu.py

--- handlers/user/user_menu.py
+++ handlers/user/user_menu.py
@@ -1,1 +1,10 @@
-KeyboardButton("📣 لینک دعوت من")
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+def user_main_menu():
+    buttons = [
+        [KeyboardButton("📡 دریافت کانفیگ رایگان")],
+        [KeyboardButton("📤 اهدای کانفیگ"), KeyboardButton("🎁 رفرال")],
+        [KeyboardButton("📥 کلاینت‌ها"), KeyboardButton("📈 وضعیت حساب")],
+        [KeyboardButton("📬 ارتباط با پشتیبانی")]
+    ]
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
